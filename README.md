c5b4a695e2cff6725278bc27923d45b8fa1fbcf5
Whitney Young <wbyoung@fadingred.com>
2014-03-11 16:15:55 -0700

    Abstracting module definition.

--- README.md
+++ README.md
@@ -1,63 +1,42 @@
-# gulp-ember-handlebars [![NPM version][npm-image]][npm-url] [![Build status][travis-image]][travis-url]
+# gulp-ember-handlebars [![NPM version][npm-image]][npm-url] [![Build status][travis-image]][travis-url] [![NPM](https://nodei.co/npm/<package>.png)](https://nodei.co/npm/gulp-ember-handlebars/)
 > Ember Handlebars plugin for gulp 3
-[![NPM](https://nodei.co/npm/<package>.png)](https://nodei.co/npm/gulp-ember-handlebars/)
 
 ## Usage
 
-First, install `gulp-ember-handlebars` as a development dependency:
+First, install _gulp-ember-handlebars_ and [gulp-define-module] as development dependencies:
 
 ```shell
-npm install --save-dev gulp-ember-handlebars
+npm install --save-dev gulp-ember-handlebars gulp-define-module
 ```
 
-Then, use the plugin in your `gulpfile.js`:
+Then, add it to your `gulpfile.js`:
 
 ```javascript
-var handlebars = require('gulp-ember-handlebars');
+var emberHandlebars = require('gulp-ember-handlebars');
 
 gulp.task('templates', function(){
   gulp.src(['app/templates/*.hbs'])
-    .pipe(handlebars({
-      outputType: 'amd'
-     }))
-    .pipe(concat('templates.js'))
-    .pipe(gulp.dest('build/js/'));
+    .pipe(emberHandlebars())
+    .pipe(defineModule('node'))
+    .pipe(gulp.dest('build/templates/'));
 });
 ```
 
+gulp-ember-handlebars outputs a raw handlebars function, so it is likely that you will want to use [gulp-define-module] to make the handlebars template available via a specific namespace or for use with a module system. For additional usage examples, we recommend that you visit [gulp-handlebars] and [gulp-define-module].
+
+
+## Compiling to various module systems
+
+[gulp-define-module] can be used to prepare the output for use with common module systems such as AMD, Node, and CommonJS. Please see the documentation for more details on how to use that in a gulp chain.
+
+gulp-ember-handlebars makes the following available for use in the [define-module wrapper](https://github.com/wbyoung/gulp-define-module#optionswrapper):
+
+ - `emberHandlebars`: The handlebars template fully wrapped (`Ember.Handlebars.template(<%= contents %>)`).
+
+
 ## API
 
-#### options.outputType
-Type: `String`
-Default: `amd`
-
-The desired output type. One of the following:
-
-* `browser` - Produce plain JavaScript files for the browser.
-* `amd` - Produce AMD modules.
-* `cjs` - Produce CommonJS modules.
-
-### options.templateRoot
-Type: `String`
-Default: `templates`
-
-This option specifies the name of the root directory for template files.
-
-### options.namespace
-Type: `String`
-Default: `Ember.TEMPLATES`
-
-This option is only necessary when `options.outputType` is `browser`. This
-option's value is the namespace that is assigned the pre-compiled templates.
-Use dot notation (e.g. 'Ember.Templates`) for nested namespaces.
-
-### options.processName
-Type: `Function`
-Default: Strip file extension
-
-This option accepts a function which takes one argument (the template name)
-and returns a string which will be used as the key for the precompiled
-template object. By default, the filename minus the extension is used.
+### emberHandlebars(options)
 
 ### options.compilerOptions
 Type: `Object`
@@ -69,3 +48,6 @@ Compiler options to pass to `Ember.Handlebars.precompile()`.
 [travis-image]: https://secure.travis-ci.org/fuseelements/gulp-ember-handlebars.png?branch=master
 [npm-url]: https://npmjs.org/package/gulp-ember-handlebars
 [npm-image]: https://badge.fury.io/gh/fuseelements%2Fgulp-ember-handlebars.png
+
+[gulp-handlebars]: https://github.com/lazd/gulp-handlebars
+[gulp-define-module]: https://github.com/wbyoung/gulp-define-module
--- test/main.js
+++ test/main.js
@@ -17,27 +17,6 @@ var getFixture = function (filePath) {
   });
 };
 
-var getFixtureWindowsStyle = function (filePath) {
-  filePath = path.join('test', 'fixtures', filePath);
-  var file = new gutil.File({
-    path: filePath,
-    cwd: path.join('test', 'fixtures'),
-    base: path.join('test'),
-    contents: fs.readFileSync(filePath)
-  });
-  Object.defineProperty(file, 'relative', {
-    get: function () {
-      if (!this.base) throw new Error('No base specified! Can not get relative.');
-      if (!this.path) throw new Error('No path specified! Can not get relative.');
-      return path.relative(this.base, this.path).replace(/\//g, '\\');
-    },
-    set: function () {
-      throw new Error('You cannot do that!');
-    }
-  });
-  return file;
-};
-
 var getExpectedString = function (filePath) {
   return fs.readFileSync(path.join('test', 'expected', filePath), 'utf8');
 };
@@ -49,94 +28,14 @@ var fileMatchesExpected = function (file, expectedFileName) {
 describe('gulp-ember-handlebars', function () {
   describe('plugin()', function () {
 
-    it('should compile templates for the browser', function (done) {
-      var stream = plugin({
-        namespace: 'NAMESPACE',
-        outputType: 'browser',
-        templateRoot: 'fixtures'
-      });
-
+    it('should compile templates', function (done) {
+      var stream = plugin();
       var basicTemplate = getFixture('Basic.hbs');
 
       stream.on('data', function (newFile) {
         should.exist(newFile);
         should.exist(newFile.contents);
-        fileMatchesExpected(newFile, 'Basic_browser.js');
-        done();
-      });
-      stream.write(basicTemplate);
-      stream.end();
-    });
-
-    it('should compile templates for AMD', function (done) {
-      var stream = plugin({
-        outputType: 'amd',
-        templateRoot: 'fixtures'
-      });
-
-      var basicTemplate = getFixture('Basic.hbs');
-
-      stream.on('data', function (newFile) {
-        should.exist(newFile);
-        should.exist(newFile.contents);
-        fileMatchesExpected(newFile, 'Basic_amd.js');
-        done();
-      });
-      stream.write(basicTemplate);
-      stream.end();
-    });
-
-    it('should compile templates for CommonJS', function (done) {
-      var stream = plugin({
-        outputType: 'cjs',
-        templateRoot: 'fixtures'
-      });
-
-      var basicTemplate = getFixture('Basic.hbs');
-
-      stream.on('data', function (newFile) {
-        should.exist(newFile);
-        should.exist(newFile.contents);
-        fileMatchesExpected(newFile, 'Basic_commonjs.js');
-        done();
-      });
-      stream.write(basicTemplate);
-      stream.end();
-    });
-
-    it('should support custom processName functions', function (done) {
-      var stream = plugin({
-        outputType: 'browser',
-        namespace: 'NAMESPACE',
-        processName: function (name) {
-          return 'x';
-        }
-      });
-
-      var basicTemplate = getFixture('Basic.hbs');
-
-      stream.on('data', function (newFile) {
-        should.exist(newFile);
-        should.exist(newFile.path);
-        newFile.path.should.equal('test/fixtures/x.js');
-        done();
-      });
-      stream.write(basicTemplate);
-      stream.end();
-    });
-
-    it('should convert windows path separators to unix path separators', function (done) {
-      var stream = plugin({
-        outputType: 'browser',
-        namespace: 'NAMESPACE'
-      });
-
-      var basicTemplate = getFixtureWindowsStyle('nested/Basic.hbs');
-
-      stream.on('data', function (newFile) {
-        should.exist(newFile);
-        should.exist(newFile.contents);
-        fileMatchesExpected(newFile, 'Basic_nested.js');
+        fileMatchesExpected(newFile, 'Basic.js');
         done();
       });
       stream.write(basicTemplate);
@@ -144,11 +43,7 @@ describe('gulp-ember-handlebars', function () {
     });
 
     it('should trigger error handler on malformed template', function (done) {
-      var stream = plugin({
-        outputType: 'browser',
-        namespace: 'NAMESPACE'
-      });
-
+      var stream = plugin();
       var malformedTemplate = getFixture('Malformed.hbs');
 
       stream.on('error', function (error) {
@@ -157,6 +52,5 @@ describe('gulp-ember-handlebars', function () {
       stream.write(malformedTemplate);
       stream.end();
     });
-
   });
 });
